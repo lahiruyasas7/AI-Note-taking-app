5aece2e84fb22078210b09151e0f92f2bb551b36
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-03-30 16:06:23 +0530

    added themeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "@/styles/globals.css";
+import { ThemeProvider } from "@/providers/themeProvider";
 
 export const metadata: Metadata = {
   title: "Lahiru's Notes",
@@ -12,8 +13,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        {" "}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
