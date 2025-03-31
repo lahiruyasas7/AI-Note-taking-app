9df3bc7bfb2002511659d918ca12973a61e47d84
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-03-31 19:57:47 +0530

    feat: created header

---
+++ src/components/Header.tsx
@@ -0,0 +1,47 @@
+import { shadow } from "@/styles/utils";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import LogoutButton from "./LogoutButton";
+
+const Header = () => {
+  const user = null;
+  return (
+    <header
+      className="relative flex h-24 w-full items-center justify-between bg-popover px-3 sm:px-8"
+      style={{ boxShadow: shadow }}
+    >
+      <Link href="/" className="flex items-end gap-2">
+        <Image
+          src="/goatius.png"
+          alt="Logo"
+          width={60}
+          height={60}
+          className="rounded-full"
+          priority
+        />
+        <h1 className="flex flex-col pb-1 text-2xl font-semibold leading-6">
+          Goat <span>Notes</span>
+        </h1>
+      </Link>
+      <div className="flex items-center gap-4">
+        {!user ? (
+          <LogoutButton/>
+        ) : (
+          <>
+            {" "}
+            <Button asChild>
+              <Link href="/sign-up">Sign up</Link>
+            </Button>
+            <Button asChild>
+              <Link href="/login">Login</Link>
+            </Button>
+          </>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
