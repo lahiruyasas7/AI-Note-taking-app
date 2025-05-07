7c8ea3679e8ac43f2d83f8f1d300bab51820bc6d
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-05-07 22:03:56 +0530

    added router to sign out button

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -3,24 +3,30 @@ import React, { useState } from "react";
 import { Button } from "./ui/button";
 import { Loader2 } from "lucide-react";
 import { Toaster } from "./ui/sonner";
+import { logOutAction } from "@/actions/users";
+import { useRouter } from "next/navigation";
 
 const LogoutButton = () => {
-
+  const router = useRouter();
   const [loading, setLoading] = useState(false);
 
-  const handleLogout = () => {
+  const handleLogout = async () => {
     setLoading(true);
-    setTimeout(()=>{
-        setLoading(false);
+    setTimeout(() => {
+      setLoading(false);
     }, 1000);
 
-    const errorMessage = "Error in Logout";
+    const { errorMessage }: any = await logOutAction();
 
-    if (errorMessage){
-        <Toaster/>
+    if (!errorMessage) {
+      router.push(`/?toastType=logOut`);
+    } else {
+      <Toaster />;
     }
   };
-  return <Button onClick={handleLogout}>{loading ? <Loader2 /> : "Log out"}</Button>;
+  return (
+    <Button onClick={handleLogout}>{loading ? <Loader2 /> : "Log out"}</Button>
+  );
 };
 
 export default LogoutButton;
